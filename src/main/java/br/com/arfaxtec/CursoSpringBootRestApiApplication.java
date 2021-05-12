9fe74abb0125ca0878a4ccaa46ecb4d30d37cf6b
Leandro Amorim <leandroamorimpb@gmail.com>
2021-05-12 16:08:08 -0300

    Primeiro commit e configurando o pom.xml do projeto

---
+++ src/main/java/br/com/arfaxtec/CursoSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arfaxtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arfaxtec/CursoSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arfaxtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
