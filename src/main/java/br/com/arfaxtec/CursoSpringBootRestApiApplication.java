bb1036fefbad2ea8c280b27149de0a0a27a9bb3b
Leandro Amorim <leandroamorimpb@gmail.com>
2021-05-12 18:31:49 -0300

    Configurando a classe main do projeto e criando os pacotes

--- src/main/java/br/com/arfaxtec/CursoSpringBootRestApiApplication.java
+++ src/main/java/br/com/arfaxtec/CursoSpringBootRestApiApplication.java
@@ -1,9 +1,23 @@
 package br.com.arfaxtec;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.com.arfaxtec.apirest.model"})//ler todas as classes desse pacote
+@ComponentScan(basePackages = {"br.com.*"})
+@EnableJpaRepositories(basePackages = {"br.com.arfaxtec.apirest.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration//o spring configura todo o projeto automaticamente
 public class CursoSpringBootRestApiApplication {
 
 	public static void main(String[] args) {
