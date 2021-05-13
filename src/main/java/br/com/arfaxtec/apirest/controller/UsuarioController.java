b261c1142eb81ac1d2258c211404f4b0b61c69e4
Leandro Amorim <leandroamorimpb@gmail.com>
2021-05-13 09:11:08 -0300

    Retornando um usuario e uma lista de usuarios em json para a tela

--- src/main/java/br/com/arfaxtec/apirest/controller/UsuarioController.java
+++ src/main/java/br/com/arfaxtec/apirest/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package br.com.arfaxtec.apirest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.arfaxtec.apirest.model.Usuario;
 
 @Controller
 @RequestMapping(value = "/system")
@@ -20,7 +25,6 @@ public class UsuarioController {
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity init(@RequestParam (value = "nome", defaultValue = "valorpadrão") String nome) {
 		
-		System.out.println("Parametro sendo recebido!!!");
 		return new ResponseEntity("Olá Usuário Rest Spring Boot, seu nome é : " + nome, HttpStatus.OK);
 	} 
 	
@@ -33,7 +37,42 @@ public class UsuarioController {
 	@GetMapping(value = "/usuario", produces = "application/json")
 	public ResponseEntity testeInit(@RequestParam (value = "nome", required = true, defaultValue = "Informe um nome!") String nome) {
 		
-		System.out.println("Parametro sendo recebido!!!");
 		return new ResponseEntity("Olá Usuário Rest Spring Boot, seu nome é : " + nome, HttpStatus.OK);
 	} 
+	
+	@SuppressWarnings("rawtypes")
+	@GetMapping(value = "/user", produces = "application/json")
+	public ResponseEntity retornaUsuario() {
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("Leandro");
+		usuario.setSenha("123");
+		
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	} 
+	
+	@SuppressWarnings("rawtypes")
+	@GetMapping(value = "/listuser", produces = "application/json")
+	public ResponseEntity retornaListaUsuarios() {
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("Leandro");
+		usuario.setSenha("123");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(1L);
+		usuario2.setLogin("Leandro");
+		usuario2.setSenha("123");
+		
+		List<Usuario> lista = new ArrayList<>();
+		
+		lista.add(usuario);
+		lista.add(usuario2);
+		
+		return new ResponseEntity(lista, HttpStatus.OK);
+	} 
+	
+	
 }
